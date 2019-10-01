6f46e569cf8ae0e2d41f720581616fc7817250d3
Eleriad <frouffet@gmail.com>
2019-10-01 22:55:50 +0200

    Cat

--- views/admin/categories.php
+++ views/admin/categories.php
@@ -10,27 +10,48 @@ if(isset($_POST['categorie']))
     {
         $newcat = $_POST['categorie'];
         $req = $bdd->prepare('INSERT INTO categories(nom) VALUES(:newcat)');
-    $req-> execute(array(
-    'newcat' => $newcat));
+        $req-> execute(array(
+        'newcat' => $newcat));
+        $req->closeCursor ();
         echo "categorie ajoutée";
     }
 
 
 //modification d'une categorie
 if(isset($_POST['newcategorie']))
+    if($_POST['cat'] != null)
     {
         $newname = $_POST['newcategorie'];
         $id = $_POST['cat'];
         $req = $bdd->prepare('UPDATE categories SET nom=:newname WHERE id=:id');
         $req-> execute(array(
         'newname' => $newname , 'id' => $id));
-        echo "categorie modifiée";
+        $req->closeCursor ();
+        echo "catégorie modifiée";
+    }
+    else
+    {
+        echo "catégorie inconnue";
     }
 
 
-// affichage des categories
-$reponse = $bdd->query('SELECT * FROM `categories` ORDER BY nom ASC');
-
+//suppression d'une categorie
+if(isset($_POST['catfin']))
+    if($_POST['catfin'] != null)
+    {
+        $id = $_POST['catfin'];
+        $req = $bdd->prepare('DELETE FROM categories WHERE id=:id');
+        $req-> execute(array('id' => $id));
+        $req->closeCursor ();
+        if($req)
+            {
+                 echo "categorie supprimée";
+            }
+    }
+    else
+    {
+        echo "catégorie inconnue";
+    }
 ?>
 
 
@@ -47,15 +68,20 @@ $reponse = $bdd->query('SELECT * FROM `categories` ORDER BY nom ASC');
 <!-- modification des listes -->
 <h2>Gérer les catégories</h2>
 
+
+<!-- renommer une catégorie -->
+<h3>Renommer une catégoie</h3>
 <form action='#' method="POST">
     <div id="listecategories">
         <select id='categorie' name='cat'>
-           <option value="---">---</option>
+           <option value="">---</option>
             <?php
+            $reponse = $bdd->query('SELECT * FROM `categories` ORDER BY nom ASC');
             while($donnees = $reponse->fetch())
-                  {
+                {
                     echo '<option value="'.$donnees['ID'].'">' . $donnees['nom'] . '</option>' ;
-                  }
+                }
+            $reponse->closeCursor ();
             ?>
         </select>
     </div>
@@ -64,7 +90,27 @@ $reponse = $bdd->query('SELECT * FROM `categories` ORDER BY nom ASC');
         <input type="text" value="" name="newcategorie" placeholder="renommer la catégorie"/>
         <button type="submit">Valider</button>
     </div>
+</form>
 
-    <div id="supprimercategiore">
+
+<!-- supprimer une catégorie -->
+<h3>Supprimer une catégoie</h3>
+<form action='#' method="POST">
+    <div id="categoriesfinales">
+        <select id='catfin' name='catfin'>
+           <option value="">---</option>
+            <?php
+            $reponse = $bdd->query('SELECT * FROM `categories` ORDER BY nom ASC');
+            while($donnees = $reponse->fetch())
+                {
+                    echo '<option value="'.$donnees['ID'].'">' . $donnees['nom'] . '</option>' ;
+                }
+            $reponse->closeCursor ();
+            ?>
+        </select>
+    </div>
+
+    <div id="supprimercategorie">
+        <button type="submit">Supprimer la catégorie</button>
     </div>
 </form>
